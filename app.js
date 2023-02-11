c340df49822368f13545e92c4240d14633fb1105
Daniel <tsuneokomido@gmail.com>
2023-02-11 08:18:50 -0300

    feat: auth facebook

--- app.js
+++ app.js
@@ -1,57 +1,97 @@
-const express         =     require('express')
-  , passport          =     require('passport')
-  , cookieParser      =     require('cookie-parser')
-  , session           =     require('express-session')
-  , bodyParser        =     require('body-parser')
-  , config            =     require('./configuration/config')
-  , app               =     express();
+const express = require("express"),
+  passport = require("passport"),
+  cookieParser = require("cookie-parser"),
+  session = require("express-session"),
+  bodyParser = require("body-parser"),
+  config = require("./configuration/config"),
+  configFace = require("./configuration/facebookConfig"),
+  app = express();
 
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-passport.use(new GoogleStrategy({
-    clientID: config.api_key,
-    clientSecret: config.api_secret,
-    callbackURL: config.callback_url
-  },
-  function(accessToken, refreshToken, profile, done) {
-    return done(null, profile);
-  }
-));
+var FacebookStrategy = require("passport-facebook").Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: config.api_key,
+      clientSecret: config.api_secret,
+      callbackURL: config.callback_url,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: configFace.api_key,
+      clientSecret: configFace.api_secret,
+      callbackURL: configFace.callback_url,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
 
 // Passport session setup.
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function(obj, done) {
+passport.deserializeUser(function (obj, done) {
   done(null, obj);
 });
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(req, res){
-  res.render('index', { user: req.user });
+app.get("/", function (req, res) {
+  res.render("index", { user: req.user });
 });
+// Google
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
-
-app.get('/auth/google/callback', 
-  passport.authenticate('google',  { successRedirect : '/', failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  }),
+  function (req, res) {
+    res.redirect("/");
   }
 );
 
-app.get('/logout', function(req, res){
+// Facebook
+app.get("/auth/facebook", passport.authenticate("facebook"));
+
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
+
+app.get("/logout", function (req, res) {
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 });
 
-app.listen(3000, () => console.log('Server up'));
+app.listen(3000, () => console.log("Server up"));
---
+++ configuration/facebookConfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+  api_key: "634275698465252",
+  api_secret: "7d0e8503f5cbd97c2b7874fe03554d05sss",
+  callback_url: "http://localhost:3000/auth/facebook/callback",
+};
